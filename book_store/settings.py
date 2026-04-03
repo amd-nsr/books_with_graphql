308d54f83eb8168dca012a2f2235eeeef5cdc3e2
amd_nsr <a.abdelnasser@florinz.com>
2026-04-04 01:31:30 +0400

    update DB

--- book_store/settings.py
+++ book_store/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_db',
+        'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': '5432',
+        'PORT': os.getenv("DB_PORT", '5432'),
     }
 }
 
